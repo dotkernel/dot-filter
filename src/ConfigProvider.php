297b62f39481cf1cb48639c9135d1d004b7c3571
n3vrax <n3vrax@gmail.com>
2017-03-11 04:09:29 +0200

    Updated php file headers doc blocks

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * @copyright: DotKernel
- * @library: dot-filter
- * @author: n3vrax
- * Date: 1/24/2017
- * Time: 11:11 PM
+ * @see https://github.com/dotkernel/dot-filter/ for the canonical source repository
+ * @copyright Copyright (c) 2017 Apidemia (https://www.apidemia.com)
+ * @license https://github.com/dotkernel/dot-filter/blob/master/LICENSE.md MIT License
  */
 
 declare(strict_types = 1);
